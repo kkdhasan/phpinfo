3b3c1b477ef9a7f3645b96f52e39fea8d886769f
kkdhasan <31755537+kkdhasan@users.noreply.github.com>
2018-04-16 22:34:38 -0700

    4

--- php/index.php
+++ php/index.php
@@ -1,16 +1,12 @@
 
 <?php
-<<<<<<< HEAD
+
 echo rand() . "\n";
 echo rand() . "\n";
 
 echo rand(5, 15);
 echo "<br>";
 echo "Platform web";
-=======
-echo "Random number generator";
-echo "Platform web team";
-?>
->>>>>>> 902fc343f7426168f1af9ddb821453f383ed713a
+
 
 ?>
